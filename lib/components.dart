2f309ffa85f16546dc806399e5d9c167bc138a8b
RealYoungK <real_youngk@naver.com>
2021-03-27 12:03:32 +0900

    current play

---
+++ lib/components.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const ActiveStar = Icon(
+  Icons.star,
+  size: 9.0,
+  color: Colors.yellow,
+);
+
+const InActiveStar = Icon(
+  Icons.star,
+  size: 9.0,
+  color: Colors.grey,
+);
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/components.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('test'),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(16, 43, 0, 0),
+              child: Text(
+                '현재 상영중',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 16.0),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.network(
+                          'https://images.chosun.com/resizer/zi79UJVrltMbR26raUDUXS2FcN0=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/BTPIRBOSBVT7GEVINZLYBL7A4Y.jpg',
+                          height: 159.17,
+                          width: 104.0,
+                        ),
+                      ),
+                      Text(
+                        '미나리',
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          ActiveStar,
+                          ActiveStar,
+                          ActiveStar,
+                          InActiveStar,
+                          InActiveStar,
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
